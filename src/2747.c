9f1ab8928d2f29ad4d2c90506a29ddbb0501d490
Minjae Seon <ddol0225@gmail.com>
2020-10-20 20:06:36 +0900

    Added Problem 2747

---
+++ src/2747.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int dp[46] = { 0 };
+
+int fibo(int n) {
+	dp[0] = 0;
+	dp[1] = 1;
+	for (int i = 2; i <= n; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+	return dp[n];
+}
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	printf("%d", fibo(n));
+}
